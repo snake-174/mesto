4c0d58d4daa4a5c2f17f7cd395bc57daccc26d6f
Artem Avdeev <174.snake@gmail.com>
2020-10-29 11:12:29 +0500

    ver 1.1

--- script/script.js
+++ script/script.js
@@ -1,31 +1,32 @@
 let editButton= document.querySelector('.profile__edit-button');
 let popup= document.querySelector('.popup');
 let closeButton= popup.querySelector('.popup__close-button');
-let saveButton= popup.querySelector('.popup__save-button');
+let form= popup.querySelector('.popup__form');
 let inputName= popup.querySelector('.popup__input_type_name');
 let inputProfession= popup.querySelector('.popup__input_type_profession');
 let name= document.querySelector('.profile__user-name');
 let profession = document.querySelector('.profile__user-profession');
 
-function formSubmitHandler (evt) {
-    evt.preventDefault(); 
-    name.textContent = inputName.value;
-    profession.textContent = inputProfession.value;
-    popup.classList.remove('popup_open');
-}
-
-function open(){ 
+function openPopup(){ 
     popup.classList.add('popup_open');
     inputName.value = name.textContent;
     inputProfession.value = profession.textContent;
 }
 
-function close(){ 
+function closePopup(){ 
     popup.classList.remove('popup_open');
 }
 
-editButton.addEventListener('click', open);
-closeButton.addEventListener('click', close);
-saveButton.addEventListener('click', formSubmitHandler) 
+function formSubmitHandler (evt) {
+    evt.preventDefault(); 
+    name.textContent = inputName.value;
+    profession.textContent = inputProfession.value;
+    closePopup();
+}
+
+editButton.addEventListener('click', openPopup);
+closeButton.addEventListener('click', closePopup);
+form.addEventListener('submit', formSubmitHandler);
+
 
 
